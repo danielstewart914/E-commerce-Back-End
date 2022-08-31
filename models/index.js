7ae10b38132ca8d80802eee16e18af1b90819725
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-08-31 08:57:05 -0700

    Added model associations in model index

--- models/index.js
+++ models/index.js
@@ -4,13 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany( Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+} );
+
+// Products belongsTo Category
+Product.belongsTo( Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+} );
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany( Tag, {
+  through: ProductTag,
+  as: 'tags'
+} );
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany( Product, {
+  through: ProductTag,
+  as: 'products'
+} );
 
 module.exports = {
   Product,
